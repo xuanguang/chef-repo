cb3323f27d51ec222176d0c69eddb11317756c50
xuanguang <lvyiguang@163.com>
2017-08-29 07:25:57 +0000

    +

--- cookbooks/my_cookbook/recipes/default.rb
+++ cookbooks/my_cookbook/recipes/default.rb
@@ -3,3 +3,6 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+
+meesage = node['my_cookbook']['message']
+chef::Log.info("** Saying what I was told to say: #{message}")
